04659d504102c48c8710680657045cf3be5a5753
favorsyoon <favorsyoon@gmail.com>
2023-01-02 11:46:18 +0900

    디폴트백만포인트pull하기전까지

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const CustomerController = require('../controllers/customer.controller');
 const customerController = new CustomerController();
 
-
 // router.get('/', postsController.getPosts);
 // router.post('/', postsController.createPost);
 
